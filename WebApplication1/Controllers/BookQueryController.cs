92d0f3e574cbbbd549addf85bcdf7a0026a650db
jupiter0306 <847999667@qq.com>
2019-05-13 21:43:26 +0800

    增加了历史订单查询界面

---
+++ WebApplication1/Controllers/BookQueryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BookQueryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Condition()
+        {
+            ViewData["Message"] = "Condition";
+            return View();
+        }
+        public IActionResult Result()
+        {
+            ViewData["Message"] = "Result";
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/PurchaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OrderQuery()
+        {
+            ViewData["Message"] = "OrderQuery";
+            return View();
+        }
+    }
+}
