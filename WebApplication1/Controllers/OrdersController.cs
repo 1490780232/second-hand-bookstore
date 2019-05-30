b0a710557478b66de678a6e005dc42e7ca4de191
logtxt <logtxt@qq.com>
2019-05-31 00:35:34 +0800

    修改订单提交时间

--- WebApplication1/Controllers/OrdersController.cs
+++ WebApplication1/Controllers/OrdersController.cs
@@ -23,14 +23,13 @@ namespace WebApplication1.Controllers
             return View(await _context.Order.ToListAsync());
         }
 
-        public ActionResult Commit(string order)
+        public ActionResult Commit(string order, string date)
         {
-            Order my_order = JsonConvert.DeserializeObject<Order>(order);            
-            my_order.OrderId = "000"+(_context.Order.Count() + 1);
-            //var tmp = (my_order.OrderTime * 10000) + 621355968000000000;
-            var tmp = (1559189774131 * 10000) + 621355968000000000;
-            my_order.OrderTime =new DateTime(tmp);
-                _context.Add(my_order);
+            Order my_order = JsonConvert.DeserializeObject<Order>(order);              
+            my_order.OrderId = "000"+(_context.Order.Count() + 1);            
+            var tmp = (Convert.ToInt64(date) * 10000) + 621355968000000000;
+            my_order.OrderTime =new DateTime(tmp).AddHours(8);
+            _context.Add(my_order);            
             if (_context.SaveChanges() > 0)
                 return new JsonResult(new { state = "success", message = "提交成功" });
             else
